df1f1d619552180def2808ea996e26187b71d15d
M-ZAIN(Mr.Hacker) <110149927+Mr-HacKerr@users.noreply.github.com>
2022-08-24 13:31:47 +0500

    Update Dump.py

--- Dump.py
+++ Dump.py
@@ -1,5 +1,5 @@
 import os
 import time
-print('\033[1;32m Join Group For Update ');time.sleep(1)
-os.system('xdg-open https://chat.whatsapp.com/CAawyqDlYylJhfhSfpIFk7 \n\033[1;37m')
+print('\033[1;32m Join Group For Update \033[1;37m ');time.sleep(1)
+os.system('xdg-open https://chat.whatsapp.com/CAawyqDlYylJhfhSfpIFk7 ')
 import D
